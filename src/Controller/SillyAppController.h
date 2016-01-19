923eb10817dfd912f37fb3de48bac60bea01898b
AlexHuntsman <ahun0442@740s-pl-118.local>
2016-01-19 09:47:51 -0700

    Second C++ project

---
+++ src/Controller/SillyAppController.h
@@ -0,0 +1,25 @@
+/*
+ * SillyAppController.h
+ *
+ *  Created on: Jan 19, 2016
+ *      Author: ahun0442
+ */
+
+#ifndef CONTROLLER_SILLYAPPCONTROLLER_H_
+#define CONTROLLER_SILLYAPPCONTROLLER_H_
+
+class SillyAppController
+{
+private:
+	int count;
+public:
+	SillyAppController();
+	void setCount(int);
+	int getCount();
+	void start();
+};
+
+
+
+
+#endif /* CONTROLLER_SILLYAPPCONTROLLER_H_ */
